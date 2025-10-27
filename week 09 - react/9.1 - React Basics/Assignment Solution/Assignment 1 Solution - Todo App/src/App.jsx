19c3ff15522970c2e7351d77df02c0fd07249d98
Akhilesh <akhileshbisht2020@gmail.com>
2025-10-27 20:49:35 +0530

    tital added

--- week 09 - react/9.1 - React Basics/Assignment Solution/Assignment 1 Solution - Todo App/src/App.jsx
+++ week 09 - react/9.1 - React Basics/Assignment Solution/Assignment 1 Solution - Todo App/src/App.jsx
@@ -2,11 +2,12 @@
 import Todo from "./Todo";
 
 function App() {
-    return (
-        <div>
-            <Todo /> {/* Render the Todo component */}
-        </div>
-    );
+  return (
+    <div>
+      <h1>todo app</h1>
+      <Todo /> {/* Render the Todo component */}
+    </div>
+  );
 }
 
 export default App;
