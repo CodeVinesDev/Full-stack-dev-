ae5332495e837c9d2055caa472c4ebad9b338a14
Akhilesh <akhileshbisht2020@gmail.com>
2025-11-04 22:49:16 +0530

    new task fetchdata

---
+++ week 09 - react/9.1 - React Basics/9.2 - React useState/Assignment Solution/Assignment 2 Solution - Fetching User Data/src/App.jsx
@@ -0,0 +1,10 @@
+// Import the UserData component from the UserData file
+import UserData from "./UserData";
+
+// Define the App component
+function App() {
+  return <div>{/* Render the UserData component inside the div */}</div>;
+}
+
+// Export the App component as the default export to be used in other components
+export default App;
