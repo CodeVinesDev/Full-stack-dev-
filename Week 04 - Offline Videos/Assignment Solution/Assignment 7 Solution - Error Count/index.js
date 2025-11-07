2743def7b9160bbbf7924ba2fd00664704cc1978
Akhilesh <akhileshbisht2020@gmail.com>
2025-11-07 22:46:33 +0530

    fix

--- Week 04 - Offline Videos/Assignment Solution/Assignment 7 Solution - Error Count/index.js
+++ Week 04 - Offline Videos/Assignment Solution/Assignment 7 Solution - Error Count/index.js
@@ -19,32 +19,32 @@ let errorCount = 0;
 
 // create a route for GET request on /user path
 app.get("/user", function (req, res) {
-    // throw an error with message "Some Error"
-    throw new Error("Some Error");
+  // throw an error with message "Some Error"
+  throw new Error("Some Error");
 
-    // return a json response with name as john if there is no error in the code execution
-    res.status(200).json({ name: "john" });
+  // return a json response with name as john if there is no error in the code execution
+  res.status(200).json({ name: "john" });
 });
 
 // create a route for POST request on /user path
 app.post("/user", function (req, res) {
-    // return a json response with message "created dummy user"
-    res.status(200).json({ msg: "created dummy user" });
+  // return a json response with message "created dummy user"
+  res.status(200).json({ msg: "created dummy user" });
 });
 
 // create a route for GET request on /errorCount path
 app.get("/errorCount", function (req, res) {
-    // return a json response with errorCount variable value
-    res.status(200).json({ errorCount });
+  // return a json response with errorCount variable value
+  res.status(200).json({ errorCount });
 });
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
-    // send a 404 status code as response to the user if there is an exception in any endpoint 
-    res.status(404).send({});
+  // send a 404 status code as response to the user if there is an exception in any endpoint!
+  res.status(404).send({});
 
-    // increment the errorCount variable by 1
-    errorCount++;
+  // increment the errorCount variable by 1
+  errorCount++;
 });
 
 // Start the server on port 3000
