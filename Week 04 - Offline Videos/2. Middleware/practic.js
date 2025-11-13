3eb2e6a5c4a2e9b68870e12d174e2ff65923ec4b
Akhilesh <akhileshbisht2020@gmail.com>
2025-11-13 20:59:42 +0530

    add middleware

--- Week 04 - Offline Videos/2. Middleware/practic.js
+++ Week 04 - Offline Videos/2. Middleware/practic.js
@@ -3,8 +3,9 @@ const app = express;
 
 const checkValidatiom = (req, res, next) => {
   let req = req.path;
-  console.log("date is", new Date.now());
-  console.log("method is", req.method);
+  console.log("date is now", new Date.now());
+  console.log("method is this", req.method);
+  //  call the next func
   next();
 };
 
