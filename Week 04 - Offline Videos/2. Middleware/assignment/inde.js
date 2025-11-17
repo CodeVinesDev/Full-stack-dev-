88ad4bcbb4c31f19dae104af5d9aa9b51880fb5b
Akhilesh <akhileshbisht2020@gmail.com>
2025-11-17 20:47:36 +0530

    fix error

--- Week 04 - Offline Videos/2. Middleware/assignment/inde.js
+++ Week 04 - Offline Videos/2. Middleware/assignment/inde.js
@@ -86,3 +86,50 @@ app.get("/ride2", isOldEnoughMiddleware, function (req, res) {
 // Start the server on port 3000
 app.listen(3000);
 */
+
+// import express module using require function and store it in express variable
+// const express = require("express");
+
+// // create an express application using express function
+// const app = express();
+
+// // Function that returns a boolean value if the age of the person is greater than 14
+// function isOldEnough(age) {
+//   // if age is greater than 14 return true, else return false
+//   if (age > 14) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// // create a route for GET request on /files path
+// app.get("/ride1", function (req, res) {
+//   // if the age is enough to ride the ride 1, return a json response with message "You have successfully riden the ride 1" else return a status code 411 with message "Sorry your age is not enough to ride the ride 1"
+//   if (isOldEnough(req.query.age)) {
+//     res.json({
+//       message: "You have successfully riden the ride 1",
+//     });
+//   } else {
+//     res.status(411).json({
+//       message: "Sorry your age is not enough to ride the ride 1",
+//     });
+//   }
+// });
+
+// // create a route for GET request on /files path
+// app.get("/ride2", function (req, res) {
+//   // if the age is enough to ride the ride 2, return a json response with message "You have successfully riden the ride 2" else return a status code 411 with message "Sorry your age is not enough to ride the ride 2"
+//   if (isOldEnough(req.query.age)) {
+//     res.json({
+//       message: "You have successfully riden the ride 2",
+//     });
+//   } else {
+//     res.status(411).json({
+//       message: "Sorry your age is not enough to ride the ride 2",
+//     });
+//   }
+// });
+
+// // Start the server on port 3000
+// app.listen(3000);
