7de6707cfe10d40f56d1dc448b71ffe1186840bf
Akhilesh <akhileshbisht2020@gmail.com>
2025-11-19 21:21:08 +0530

    complete notfication ui

--- week 09 - react/9.3 - React From Basic Part 1 (Recorded)/Assignment Solution/src/App.jsx
+++ week 09 - react/9.3 - React From Basic Part 1 (Recorded)/Assignment Solution/src/App.jsx
@@ -1,27 +1,68 @@
-import PostComponent from "./PostComponent";
-import ProfileComponent from "./ProfileComponent";
+// import PostComponent from "./PostComponent";
+// import ProfileComponent from "./ProfileComponent";
 
-// Create a function component named App that will be rendered in the root element
+// // Create a function component named App that will be rendered in the root element
+// function App() {
+//   // return JSX that will be rendered
+//   return (
+//     // Apply inline styles to the div element
+//     <div style={{ backgroundColor: "#dfe6e9", minHeight: "100vh" }}>
+//       <div
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <div>
+//           <PostComponent />
+//           <ProfileComponent />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// // Export App Component to use it in other components
+// export default App;
+
+import { useEffect, useState } from "react";
+
 function App() {
-  // return JSX that will be rendered
+  const [not, setNot] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("run");
+      setNot((prev) => prev + 1);
+    }, 1000);
+
+    console.log("useEffect run");
+
+    // cleanup
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    // Apply inline styles to the div element
-    <div style={{ backgroundColor: "#dfe6e9", minHeight: "100vh" }}>
+    <>
+      <h1>NOTIFICATIONS</h1>
+
       <div
         style={{
+          background: "red",
+          width: "30px",
+          height: "30px",
+          borderRadius: "100px",
+          color: "white",
           display: "flex",
-          justifyContent: "center",
           alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <div>
-          <PostComponent />
-          <ProfileComponent />
-        </div>
+        <span>{not}</span>
       </div>
-    </div>
+    </>
   );
 }
 
-// Export App Component to use it in other components
 export default App;
