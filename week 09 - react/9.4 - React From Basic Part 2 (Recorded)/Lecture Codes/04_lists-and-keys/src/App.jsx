c891975f82df80fc01a2a55d789c30535c538169
Akhilesh <akhileshbisht2020@gmail.com>
2025-11-20 21:28:16 +0530

    pass as a props

--- week 09 - react/9.4 - React From Basic Part 2 (Recorded)/Lecture Codes/04_lists-and-keys/src/App.jsx
+++ week 09 - react/9.4 - React From Basic Part 2 (Recorded)/Lecture Codes/04_lists-and-keys/src/App.jsx
@@ -30,5 +30,31 @@ function App() {
   );
 }
 
+// Create a Todo component that will render the title and status of a todo item
+function Todo({ title, done }) {
+  // Return JSX that will be rendered
+  return (
+    // Create a div element with JSX
+    <div>
+      {/* Render the title and status of the todo item */}
+      {title} - {done ? "Done" : "Not done"}
+    </div>
+  );
+}
+
+// Create an ItemList component that will render a list of items
+const ItemList = ({ items }) => {
+  // Return JSX that will be rendered
+  return (
+    // Create an unordered list element with JSX
+    <ul>
+      {/* Render a list item for each item in the items array */}
+      {items.map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+};
+
 // Export the App component as the default export to be used in other files or components
 export default App;
