93dc264765146c8bf46aed3f791c762f114627fa
Akhilesh <akhileshbisht2020@gmail.com>
2025-11-20 21:25:00 +0530

    add model

--- week 09 - react/9.4 - React From Basic Part 2 (Recorded)/Lecture Codes/02_modals/src/App.jsx
+++ week 09 - react/9.4 - React From Basic Part 2 (Recorded)/Lecture Codes/02_modals/src/App.jsx
@@ -23,6 +23,44 @@ const App = () => {
 };
 
 // Create a Modal component that will render children elements passed to it as props
+const Modal = ({ isOpen, onClose, children }) => {
+  // If the modal is not open, return null
+  if (!isOpen) {
+    return null;
+  }
+
+  // Return JSX that will be rendered in the browser
+  return (
+    // Create a div element with inline CSS
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          background: "white",
+          padding: "20px",
+          borderRadius: "5px",
+        }}
+      >
+        {/* Render a button element with an onClick event handler that calls onClose */}
+        <button onClick={onClose}>Close</button>
+
+        {/* Render any children elements passed to the Modal component */}
+        {children}
+      </div>
+    </div>
+  );
+};
 
 // Export the App component as the default export to be used in other files or components
 export default App;
