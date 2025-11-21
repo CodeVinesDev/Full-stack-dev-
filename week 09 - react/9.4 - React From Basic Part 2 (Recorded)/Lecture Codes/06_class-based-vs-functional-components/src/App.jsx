f60b6452bd5dabed8d9d30234f0c8097c5cdab84
Akhilesh <akhileshbisht2020@gmail.com>
2025-11-21 22:02:52 +0530

    inisilize fresh setup

---
+++ week 09 - react/9.4 - React From Basic Part 2 (Recorded)/Lecture Codes/06_class-based-vs-functional-components/src/App.jsx
@@ -0,0 +1,14 @@
+// Import useState and Component from react module
+import { useState, Component } from "react";
+
+// Create a functional component App that will be rendered in the root element
+function App() {
+  // Render the JSX that will be rendered in the browser
+  return (
+    // Create a div element with JSX
+    <div>{/* Render the ClassCounter component  */}</div>
+  );
+}
+
+// Export the App component as the default export to be used in other files or components
+export default App;
