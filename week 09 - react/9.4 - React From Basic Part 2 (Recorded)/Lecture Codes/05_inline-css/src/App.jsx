612f7b69518896052373a95ce9a396b1a7dde47d
Akhilesh <akhileshbisht2020@gmail.com>
2025-11-21 22:00:33 +0530

    inline style

---
+++ week 09 - react/9.4 - React From Basic Part 2 (Recorded)/Lecture Codes/05_inline-css/src/App.jsx
@@ -0,0 +1,11 @@
+// Create a function component named App that will be rendered in the root element
+function App() {
+  // Return the JSX that will be rendered in the browser
+  return (
+    // Create a div element with JSX
+    <div>{/* Render the MyComponent component */}</div>
+  );
+}
+
+// Export the App component as the default export to be used in other files or components
+export default App;
