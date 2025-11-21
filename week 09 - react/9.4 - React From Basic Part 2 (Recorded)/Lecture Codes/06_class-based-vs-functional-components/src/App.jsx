52714c7993cf06e70d5dfb1cac42f191b8742704
Akhilesh <akhileshbisht2020@gmail.com>
2025-11-21 22:03:41 +0530

    class vs function

--- week 09 - react/9.4 - React From Basic Part 2 (Recorded)/Lecture Codes/06_class-based-vs-functional-components/src/App.jsx
+++ week 09 - react/9.4 - React From Basic Part 2 (Recorded)/Lecture Codes/06_class-based-vs-functional-components/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <ClassCounter />
 
       {/* Render the ClassCounter component  */}
+      <FunctionalCounter />
     </div>
   );
 }
@@ -40,6 +41,30 @@ class ClassCounter extends Component {
     );
   }
 }
+
+// Create a functional component FunctionalCounter that will render a div element with a count property
+const FunctionalCounter = () => {
+  // Create a state object with a count property set to 0 using the useState hook
+  const [count, setCount] = useState(0);
+
+  // Create a function named increment that will increment the count property by 1
+  function increment() {
+    // Set the state of the count property to the current count plus 1 using the setCount function
+    setCount((count) => count + 1);
+  }
+
+  // Render the JSX that will be rendered in the browser
+  return (
+    // Create a div element with JSX
+    <div>
+      {/* Render the count property from the state object */}
+      <p>Count: {count}</p>
+
+      {/* Create a button element with an onClick event that will call the increment function */}
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
 
 // Export the App component as the default export to be used in other files or components
 export default App;
