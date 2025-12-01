0ef1039123a1cc97e5821448d9e71876bdc9cbd2
Akhilesh <akhileshbisht2020@gmail.com>
2025-12-01 22:44:37 +0530

    recoil

--- week-10/10.2 - React Part 3 (Context API, Rolling Up The State)/Assignment Solution/multilevel-dropdown-menu/src/App.jsx
+++ week-10/10.2 - React Part 3 (Context API, Rolling Up The State)/Assignment Solution/multilevel-dropdown-menu/src/App.jsx
@@ -1,24 +1,46 @@
-import { CounterContext, CounterProvider } from "./context/Counter";
-import { useContext } from "react";
-function App() {
-  return (
-    <>
-      <CounterProvider>
-        <Display />
-      </CounterProvider>
-    </>
-  );
-}
+// import { CounterContext, CounterProvider } from "./context/Counter";
+// import { useContext } from "react";
+// function App() {
+//   return (
+//     <>
+//       <CounterProvider>
+//         <Display />
+//       </CounterProvider>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
+
+// const Display = () => {
+//   const { minus, plus, counter } = useContext(CounterContext);
+//   return (
+//     <>
+//       <h1>{counter}</h1>
+//       <button onClick={plus}>add </button>
+//       <button onClick={minus}> decresss</button>
+//     </>
+//   );
+// };
+
+//   recoil
+
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { Counter } from "./store/Counter";
 
 const Display = () => {
-  const { minus, plus, counter } = useContext(CounterContext);
+  const counter = useRecoilValue(Counter);
+  const setCounter = useSetRecoilState(Counter);
+
   return (
     <>
       <h1>{counter}</h1>
-      <button onClick={plus}>add </button>
-      <button onClick={minus}> decresss</button>
+
+      <button onClick={() => setCounter((prev) => prev + 1)}>add</button>
+
+      <button onClick={() => setCounter((prev) => prev - 1)}>decrease</button>
     </>
   );
 };
+
+export default Display;
---
+++ week-10/10.2 - React Part 3 (Context API, Rolling Up The State)/Assignment Solution/multilevel-dropdown-menu/src/store/Counter.js
@@ -0,0 +1,5 @@
+import { atom } from "recoil";
+export const Counter = atom({
+  default: 0,
+  key: "counter",
+});
