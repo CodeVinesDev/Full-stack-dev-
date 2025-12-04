50eb16750a03a2507b08cbb7a91be738c9f97aab
Akhilesh <akhileshbisht2020@gmail.com>
2025-12-04 22:22:53 +0530

    swith toggle

--- week-10/10.2 - React Part 3 (Context API, Rolling Up The State)/Lecture Codes/04_context-api/src/App.jsx
+++ week-10/10.2 - React Part 3 (Context API, Rolling Up The State)/Lecture Codes/04_context-api/src/App.jsx
@@ -31,5 +31,26 @@ function App() {
   );
 }
 
+// Create a function component named LightSwitch that toggles the status of the bulb
+function LightSwitch() {
+  // Use the useContext hook to access the value of bulbOn and setBulbOn from the BulbContext
+  const { bulbOn, setBulbOn } = useContext(BulbContext);
+
+  // Create a function named toggleBulb that toggles the status of the bulb
+  function toggleBulb() {
+    // Update the state of the bulbOn using the setBulbOn function to toggle the status of the bulb
+    setBulbOn(!bulbOn);
+  }
+
+  // Return the JSX for the component
+  return (
+    // Display a button to toggle the status of the bulb
+    <div>
+      {/* Button to toggle the status of the bulb using the toggleBulb function */}
+      <button onClick={toggleBulb}>Toggle the Bulb</button>
+    </div>
+  );
+}
+
 // Export the App component as the default export to be used in other files or components
 export default App;
