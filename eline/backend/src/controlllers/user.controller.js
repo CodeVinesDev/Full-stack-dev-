43e09c36a38a74efaa6a6d85fb159173c9a74ea6
Akhilesh <akhileshbisht2020@gmail.com>
2025-12-08 22:51:38 +0530

    setup for backend

--- eline/backend/src/controlllers/user.controller.js
+++ eline/backend/src/controlllers/user.controller.js
@@ -1,10 +1,26 @@
-export const signup = (req, res) => {
+import { User } from "../models/User.modle";
+export const signup = async (req, res) => {
   const { email, password } = req.body;
   try {
-    if (!email || !password) {
+    if (!email || (!password && password.length >= 8)) {
       throw new Error("plese filed all form");
     }
-  } catch (error) {}
+
+    const exitUser = await User.find({ email });
+    if (exitUser) {
+      res.status(404).json("user areday exists plese login ");
+    }
+
+    const newUser = {
+      email: email,
+      password: password,
+    };
+
+    await User.create(newUser);
+    return res.status(200).json("user susecfuly signup");
+  } catch (error) {
+    throw new Error("somthing went wrong");
+  }
 };
 export const login = (req, res) => {};
 export const logout = (req, res) => {};
--- eline/backend/src/models/User.modle.js
+++ eline/backend/src/models/User.modle.js
@@ -16,4 +16,14 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await brcpt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = function (password) {
+  const compare = brcpt.compare(password, this.password);
+  if (compare) return compare;
+};
 export const User = model("User", userSchema);
