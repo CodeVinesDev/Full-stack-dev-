d32e70789d289e9f42b1933420872a4945879c5b
Akhilesh <akhileshbisht2020@gmail.com>
2026-01-22 22:36:53 +0530

    port changes

--- Week 08 - Course Selling App/8.1 - Backend of Course Selling App/Lecture Codes/Course-Selling-App/index.js
+++ Week 08 - Course Selling App/8.1 - Backend of Course Selling App/Lecture Codes/Course-Selling-App/index.js
@@ -59,7 +59,7 @@ async function main() {
     
     // Start the server on port 3000
     app.listen(3000, () => {
-        console.log("Server is listening on port 3000");
+        console.log("Server is listening on port `3000`");
     });
 }
 
