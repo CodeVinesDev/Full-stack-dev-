4de7acff20cf5766b6906ce018a9a9b385a7ff79
Akhilesh <akhileshbisht2020@gmail.com>
2026-02-05 21:35:21 +0530

    asynchronous data query

--- Week 11/11.3 - Recoil Deep Dive (Offline)/Lecture Codes/02_selectors/src/atoms.js
+++ Week 11/11.3 - Recoil Deep Dive (Offline)/Lecture Codes/02_selectors/src/atoms.js
@@ -17,36 +17,7 @@ export const jobsAtom = atom({
     default: 0,
 });
 
-// Define an atom for managing the general notification count and export it
-export const notificationsAtom = atom({
-    // Unique key for identifying the atom
-    key: 'notificationsAtom',
-    // Default value for the atom
-    default: 12,
-});
 
-// Define an atom for managing the messaging notification count and export it
-export const messagingAtom = atom({
-    // Unique key for identifying the atom
-    key: 'messagingAtom',
-    // Default value for the atom
-    default: 0,
-});
 
-// Define a selector to calculate the total notification count and export it
-export const totalNotificationsSelector = selector({
-    // Unique key for identifying the selector
-    key: 'totalNotificationsSelector',
 
-    // Define the get function to calculate the total notification count
-    get: ({ get }) => {
-        // Get the current value of the networkAtom, jobsAtom, notificationsAtom, messagingAtom atoms
-        const networkNotificationCount = get(networkAtom);
-        const jobsAtomCount = get(jobsAtom);
-        const notificationsAtomCount = get(notificationsAtom);
-        const messagingAtomCount = get(messagingAtom);
 
-        // Return the sum of the notification counts from all the atoms
-        return networkNotificationCount + jobsAtomCount + notificationsAtomCount + messagingAtomCount;
-    }
-});
